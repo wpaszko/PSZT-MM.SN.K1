69faf33700afefdb4683a254e6dba041d8878b2c
Sebastian Pietras <01133337@pw.edu.pl>
2020-01-26 16:19:29 +0100

    Added CrossEntropy loss function

---
+++ metrics.py
@@ -0,0 +1,61 @@
+"""
+Module with metrics helpers
+"""
+
+from abc import ABC, abstractmethod
+import numpy as np
+
+
+class Loss(ABC):
+    """
+    Loss base class
+    """
+
+    @abstractmethod
+    def loss(self, y_true, y_pred):
+        """
+        Calculate loss across batch
+
+        Args:
+            y_true: true labels
+            y_pred: predicted labels
+
+        Returns:
+            loss value
+        """
+        pass
+
+    @abstractmethod
+    def gradient(self, y_true, y_pred):
+        """
+        Calculate gradient of loss with respect to y_pred
+
+        Args:
+            y_true: true labels
+            y_pred: predicted labels
+
+        Returns:
+            idk yet
+        """
+        pass
+
+
+class CrossEntropyLoss(Loss):
+
+    epsilon = 1e-12
+
+    def loss(self, y_true, y_pred):
+        """
+        Computes cross entropy between true labels and predictions
+        """
+
+        predictions = np.clip(y_pred, self.epsilon, 1. - self.epsilon)
+        batch_size = predictions.shape[0]
+        ce = -np.sum(y_true * np.log(predictions)) / batch_size
+        return ce
+
+    def gradient(self, y_true, y_pred):
+        """
+        Gradient of cross entropy with relation to y_pred
+        """
+        return - y_true / y_pred
