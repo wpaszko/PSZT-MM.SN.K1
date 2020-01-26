d18086fc1b2f4577f6a8c00681408b6c822a1ce6
wpaszko <293114@pw.edu.pl>
2020-01-26 16:48:39 +0100

    Preparation of dataset

--- test.py
+++ test.py
@@ -1,8 +1,23 @@
 import models
 import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
 
-input = np.array([[1, 2, 3, 4]])
-target = np.array([0, 1])
+database = load_iris()
+
+X = database.data
+y = database.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+
+#one hot encoding on y_train array
+a = y_train
+y_train = np.zeros((a.size, a.max()+1))
+y_train[np.arange(a.size),a] = 1
+
+input = X_train
+target = y_train
 
 mlp = models.MultiLayerPerceptron(input.shape[1], target.shape[1], [4, 3])
 
