42affd1a459d98ee99d9a692e53589c3c930768b
Sebastian Pietras <01133337@pw.edu.pl>
2020-01-27 19:29:35 +0100

    Added k-cross validation

---
+++ helpers.py
@@ -0,0 +1,55 @@
+"""
+Module with helpers function
+"""
+
+import copy
+import numpy as np
+
+from sklearn.model_selection import KFold
+
+
+def cross_val_score(model, x, y, k=5, epochs=100, batch_size=1, shuffling=True):
+    """
+    Perform k-cross validation and return mean loss
+
+    Args:
+        model: model to validate
+        x: input
+        y: target
+        k: number of validations
+        epochs: iterations of learning
+        batch_size: samples per gradient update
+        shuffling: whether to randomly shuffle data in each epoch
+
+    Returns:
+        mean loss of k validations
+    """
+    losses = []
+    fresh_model = copy.deepcopy(model)
+
+    for train_idx, test_idx in KFold(k).split(x, y):
+        model = fresh_model
+
+        x_train, y_train = x[train_idx], y[train_idx]
+        x_test, y_test = x[test_idx], y[test_idx]
+
+        model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=0.0, shuffling=shuffling)
+
+        losses.append(model.evaluate(x_test, y_test))
+
+    return np.mean(losses)
+
+
+def one_hot_encode(x):
+    """
+    Turns categories into ones and zeros in each sample [2], [3] -> [0, 0, 1, 0], [0, 0, 0, 1]
+    Args:
+        x: 1d array of categories of each sample
+
+    Returns:
+        2d array of one-hot encoded categories
+    """
+    a = x
+    x = np.zeros((a.size, a.max() + 1))
+    x[np.arange(a.size), a] = 1
+    return x
--- test.py
+++ test.py
@@ -1,36 +1,22 @@
 import argparse
 import models
-import numpy as np
+import helpers
 
 from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
 
 
-def learn_and_evaluate(neurons, learn_rate, epochs, batch_size, test_size):
+def learn_and_evaluate(neurons, learn_rate, k, epochs, batch_size):
     database = load_iris()
 
     x = database.data
     y = database.target
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size)
-
-    # one hot encoding on y_train array
-    a = y_train
-    y_train = np.zeros((a.size, a.max() + 1))
-    y_train[np.arange(a.size), a] = 1
-
-    input = x_train
-    target = y_train
+    input = x
+    target = helpers.one_hot_encode(y)
 
     mlp = models.MultiLayerPerceptron(input.shape[1], target.shape[1], neurons, learn_rate=learn_rate)
 
-    mlp.fit(input, target, epochs=epochs, batch_size=batch_size)
-
-    a = y_test
-    y_test = np.zeros((a.size, a.max() + 1))
-    y_test[np.arange(a.size), a] = 1
-
-    return mlp.evaluate(x_test, y_test)
+    return helpers.cross_val_score(mlp, input, target, k, epochs, batch_size)
 
 
 def parse_args(parser):
@@ -41,12 +27,12 @@ def parse_args(parser):
             parser.error("Neuron numbers should be integers > 1")
     if not (0.0 <= args.l <= 1.0):
         parser.error("Learning rate should be between 0.0 and 1.0")
+    if not args.k >= 1:
+        parser.error("K-cross validation size should be at least one")
     if not args.e >= 1:
         parser.error("There has to be at least one epoch")
     if not args.b >= 0:
         parser.error("Batch size has to be >= 0")
-    if not (0.0 <= args.t <= 1.0):
-        parser.error("Test set size should be between 0.0 and 1.0")
 
     return args
 
@@ -55,10 +41,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="neural network classifying iris flowers")
     parser.add_argument('neurons', type=int, nargs='+', help='number of neurons on hidden layers (list)')
     parser.add_argument('-l', type=float, default=0.01, help='learning rate')
+    parser.add_argument('-k', type=int, default=5, help='k-cross validation size')
     parser.add_argument('-e', type=int, default=100, help='epochs')
     parser.add_argument('-b', type=int, default=1, help='batch size (0 - full input)')
-    parser.add_argument('-t', type=float, default=0.2, help='test set size')
     args = parse_args(parser)
 
-    loss = learn_and_evaluate(args.neurons, args.l, args.e, args.b, args.t)
+    loss = learn_and_evaluate(args.neurons, args.l, args.k, args.e, args.b)
     print("Loss value:", loss)
